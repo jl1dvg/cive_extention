a8bfe8eedcd75bcaf5a26e38625ed4d165ac4171
jorgeluisdevera <jl1dvg@gmail.com>
2024-07-17 13:52:59 -0500

    Se independiza funcion del boton protocolos y examenes

--- js/popup.js
+++ js/popup.js
@@ -1,35 +1,36 @@
-import {cargarExamenes} from './examenes.js';
-import {cargarProtocolos} from './procedimientos.js';
+import { cargarExamenes } from './examenes.js';
+import { cargarProtocolos } from './procedimientos.js';
 
 document.addEventListener("DOMContentLoaded", () => {
+    // Asignar eventos de click a los botones independientemente
     document.getElementById('btnExamenes').addEventListener('click', () => {
         console.log('Botón Exámenes clickeado');
         mostrarSeccion('examenes');
         cargarExamenes();
+    });
 
-        document.getElementById('btnProtocolos').addEventListener('click', () => {
-            console.log('Botón Protocolos clickeado');
-            mostrarSeccion('protocolos');
-            cargarProtocolos();
-        });
+    document.getElementById('btnProtocolos').addEventListener('click', () => {
+        console.log('Botón Protocolos clickeado');
+        mostrarSeccion('protocolos');
+        cargarProtocolos();
+    });
 
-        document.getElementById('btnBackExamenes').addEventListener('click', () => {
-            console.log('Botón Back Exámenes clickeado');
-            mostrarSeccion('inicio');
-        });
+    document.getElementById('btnBackExamenes').addEventListener('click', () => {
+        console.log('Botón Back Exámenes clickeado');
+        mostrarSeccion('inicio');
+    });
 
-        document.getElementById('btnBackProtocolos').addEventListener('click', () => {
-            console.log('Botón Back Protocolos clickeado');
-            mostrarSeccion('inicio');
-        });
+    document.getElementById('btnBackProtocolos').addEventListener('click', () => {
+        console.log('Botón Back Protocolos clickeado');
+        mostrarSeccion('inicio');
+    });
 
-        document.getElementById('btnAceptarOD_OI').addEventListener('click', () => {
-            const OD = document.getElementById('inputOD').value;
-            const OI = document.getElementById('inputOI').value;
-            // Aquí puedes realizar las acciones necesarias con los valores de OD y OI
-            console.log(`OD: ${OD}, OI: ${OI}`);
-            mostrarSeccion('inicio');
-        });
+    document.getElementById('btnAceptarOD_OI').addEventListener('click', () => {
+        const OD = document.getElementById('inputOD').value;
+        const OI = document.getElementById('inputOI').value;
+        // Aquí puedes realizar las acciones necesarias con los valores de OD y OI
+        console.log(`OD: ${OD}, OI: ${OI}`);
+        mostrarSeccion('inicio');
     });
 });
 
