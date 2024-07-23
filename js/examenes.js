e2a1019262581f5afbf26c0a0e6e1c0f810f424a
jorgeluisdevera <jl1dvg@gmail.com>
2024-07-23 18:04:17 -0500

    Se agrega Retino como

--- js/examenes.js
+++ js/examenes.js
@@ -352,7 +352,7 @@ OI: ${OI}`;
             }
 
             ejecutarTecnicos(item)
-                //.then(() => hacerClickEnBotonTerminar())
+                .then(() => hacerClickEnBotonTerminar())
                 .catch(error => console.log('Error en la ejecución de examen:', error));
         });
     } else if (item.id === 'cv') {
